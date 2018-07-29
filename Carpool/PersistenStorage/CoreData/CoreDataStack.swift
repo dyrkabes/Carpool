77a94be10e4ad19b965423052aa4e0c341ea6b59
Pavel Stepanov <pavel.viktorovich.stepanov@gmail.com>
2018-07-30 00:15:42 +0300

    [master] Fixed bad commit

--- Carpool/PersistenStorage/CoreData/CoreDataStack.swift
+++ Carpool/PersistenStorage/CoreData/CoreDataStack.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import CoreData
+
+final class CoreDataStack {
+    private let modelName: String
+//    private let storeType: CoreDataPersistentStoreType
+    
+    private lazy var documentDirectory: URL? = {
+        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        let storeName = modelName + ".sqlite"
+        let storeURL = documentDirectory?.appendingPathComponent(storeName)
+        return storeURL
+    }()
+    
+    lazy var context: NSManagedObjectContext = {
+        return managedObjectContext
+    }()
+    
+    private lazy var maangedObjectModel: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
+            fatalError("NSManagedObjectModel \(self.self) \(#function) could not initialized")
+        }
+        
+        guard let model = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("NSManagedObjectModel \(self.self) \(#function) could not initialized")
+        }
+        
+        return model
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: maangedObjectModel)
+        
+        do {
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType,
+                                               configurationName: nil,
+                                               at: documentDirectory,
+                                               options: nil)
+        } catch {
+            fatalError("\(error)")
+        }
+        return coordinator
+    }()
+    
+    private lazy var managedObjectContext: NSManagedObjectContext = {
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        context.persistentStoreCoordinator = persistentStoreCoordinator
+        context.mergePolicy = NSMergePolicy.overwrite
+        return context
+    }()
+    
+    // MARK: - Init
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+}
--- Carpool/Screens/TabBar/Entities/Placemark.swift
+++ Carpool/Screens/TabBar/Entities/Placemark.swift
@@ -23,7 +23,39 @@ struct Placemark: Decodable {
     let name: String
     let vin: String
     
+    // MARK: - Init
     static var empty: Placemark {
         return Placemark(address: "", coordinates: CGPoint.zero, engineType: "", exterior: .unacceptable, fuel: 0, interior: .unacceptable, name: "", vin: "")
     }
+    
+    init(address: String, coordinates: CGPoint, engineType: String, exterior: VisualCondition, fuel: Int, interior: VisualCondition, name: String, vin: String) {
+        self.address = address
+        self.coordinates = coordinates
+        self.engineType = engineType
+        self.exterior = exterior
+        self.fuel = fuel
+        self.interior = interior
+        self.name = name
+        self.vin = vin
+    }
+    
+    init?(managedObject: PlacemarkManaged) {
+        guard let address = managedObject.address,
+            let engineType = managedObject.engineType,
+            let exterior = managedObject.exterior,
+            let interior = managedObject.interior,
+            let name = managedObject.name,
+            let vin = managedObject.vin else { return nil }
+        
+        self.address = address
+        self.coordinates = CGPoint(x: managedObject.xCoordinate, y: managedObject.yCoordinate)
+        self.engineType = engineType
+        self.exterior = VisualCondition(rawValue: exterior) ?? .unacceptable
+        self.fuel = Int(managedObject.fuel)
+        self.interior = VisualCondition(rawValue: interior) ?? .unacceptable
+        self.name = name
+        self.vin = vin
+    }
 }
+
+
